fac1bba247b2ea067a2c5b7727b411ebe977c9fa
thisworksforme <87668728+thisworksforme@users.noreply.github.com>
2021-09-18 02:03:37 +0530

    Initial commit

---
+++ src/TitleComp.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TitleComp = () => {
+  return (
+    <div className="main-heading">
+      <h1>Welcome To Recipe App by Shambhavi Sahay. </h1>
+      <h5> Type the Name of a Dish and Click on Search.. </h5>
+    </div>
+  );
+};
+export default TitleComp;
